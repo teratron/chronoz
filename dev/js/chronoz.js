00b6a356e68a3e450b6d914de3a21d0c535a1e98
texatron <teratronsirimus@gmail.com>
2019-04-12 17:04:57 +0300

    less

---
+++ dev/js/chronoz.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+
+    var body = $('body');
+
+    // Dropdown
+    //-------------------------------------------------
+    $(this).on('click', '.dropdown', '[data-popup-id]', function() {
+        $(this).toggleClass('active');
+
+        if ($(this).is('[data-popup-id]')) {
+             id = $('#' + $(this).data('popup-id'));
+            var i = 0, j = 0;
+
+            id.attr('class').split(/\s+/).map(function(cls) {
+                i++;
+                switch(cls) {
+                    case 'show':
+                        id.removeClass('show');
+                        break;
+                    default: j++; break;
+                }
+            });
+            if (i == j) id.addClass('show');
+        }
+    });
+
+    // Modal / Drawer
+    //-------------------------------------------------
+    $(this).on('click', '[data-modal-id]', function() {
+        var id = $('#' + $(this).data('modal-id'));
+        var i = 0, j = 0;
+
+        id.attr('class').split(/\s+/).map(function(cls) {
+            i++;
+            switch(cls) {
+                case 'show':
+                    id.removeClass('show');
+                    body.removeClass('noscroll');
+                    break;
+                default: j++; break;
+            }
+        });
+        if (i == j) {
+            id.addClass('show');
+            body.addClass('noscroll');
+        }
+    });
+
+    // Accordeon
+    //-------------------------------------------------
+    $(this).on('click', '.accordeon-item', function() {
+
+        if ($(this).hasClass('show'))
+            $(this).removeClass('show');
+        else
+            $(this)
+                .not('.show')
+                .addClass('show')
+                .parent('.accordeon')
+                .parent()
+                .find('.accordeon-item.show')
+                .not(this)
+                .removeClass('show');
+    });
+
+    // Smooth Scroll
+    //-------------------------------------------------
+    $($('a[href*="#"]:not([href="#"])')).click(function(event) {
+        event.preventDefault();
+        //$('html, body, .content-swiper > .swiper-wrapper').animate({scrollTop:$(this.hash).offset().top}, 500);
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+        location.hash = $(this).attr('href');
+    });
+
+    // Menu Active Hash
+    //-------------------------------------------------
+    $(window).scroll(function() {
+        $('[data-hash]').each(function() {
+            if ($(this).children().visible(true, 'both')) {
+                var hash = $(this).data('hash');
+                var menu = $('#header').find('.body > menu');
+                menu.children('a[href*="#"]').removeClass('active');
+                menu.children('a[href="#' + hash + '"]').addClass('active');
+            }
+        });
+    });
+});
